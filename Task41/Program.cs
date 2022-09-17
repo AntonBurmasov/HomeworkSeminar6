bd2eb7f0a1a147e807dbfff868750cb8a15a582f
burmasovanton <looseangel1@gmail.com>
2022-09-17 23:47:46 +0500

    not working

---
+++ Task41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел
+//  больше 0 ввёл пользователь. Ввод чисел останавливается при помощи ввода 
+//  стоп-слова "stop"
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+
+void ReadNumber(string message)
+{
+ Console.WriteLine(message);
+  string  text = Console.ReadLine();
+  string stop = "stop";
+   int count = 0;
+   
+   if(text == stop)
+ {
+    Console.WriteLine($"Число больше 0 было введено {count} раз.");
+    return;
+ }
+ 
+ 
+   int number = Convert.ToInt32(Console.ReadLine());
+   
+    
+     if(number > 0)
+     {
+        count++;
+ 
+      }
+   
+ 
+  ReadNumber(message);   
+}
+
+ReadNumber("Введите число:");
+
+
