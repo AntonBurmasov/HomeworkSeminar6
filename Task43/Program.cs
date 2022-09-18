998152a126458c11f19924f8aa0c400dd2eaf2bf
burmasovanton <looseangel1@gmail.com>
2022-09-18 23:37:17 +0500

    working task 43

---
+++ Task43/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//  значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+
+
+double k1 = ReadNumber("Введите угловой коэффициент прямой 1:");
+double k2 = ReadNumber("Введите угловой коэффициент прямой 2:");
+if(k1 == k2)
+{
+  Console.WriteLine("Прямые параллельны или совпадают.");
+  return;
+}
+
+double b1 = ReadNumber("Введите пересечение с осью y прямой 1:");
+double b2 = ReadNumber("Введите пересечение с осью y прямой 2:");
+
+
+
+
+double x = (b2 - b1)/(k1 - k2);
+
+double y = k1 * x + b1;
+
+double y2 = k2 * x + b2;
+x = Math.Round(x, 2);
+y = Math.Round(y, 2);
+y2 = Math.Round(y2, 2);
+
+if(y == y2)
+{
+
+Console.WriteLine($"Точка пересечения двух прямых равна {x} по оси X и {y} по оси Y.");
+
+
+}
+
+
+
